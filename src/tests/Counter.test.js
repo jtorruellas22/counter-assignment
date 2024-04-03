b08d8442cbe52ca29c2189ac61b89e928c26bc7a
Jan Torruellas <jtorruellas@ufl.edu>
2024-04-03 16:12:36 -0400

    Update Counter.test.js
    Completed all four Unit Tests for ensuring heading and count are rendering as well as ensuring increment and decrement functionality

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,42 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen, fireEvent, getByRole } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterElement = screen.getByTestId('count');
+  expect(counterElement).toBeInTheDocument();
+  const counterRender = screen.getByText(/0/i);
+  expect(counterRender).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', async() => {
+  const incrementButton = screen.getByRole('button', {name: '+'})
+
+  await userEvent.click(incrementButton)
+  expect(Counter.increment).toHaveBeenCalled
+  expect(Counter.setCount).toHaveBeenCalled
+  
+  const counterRender = screen.getByText(/1/i);
+  expect(counterRender).toBeInTheDocument();
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking - decrements the count', async() => {
+  const decrementButton = screen.getByRole('button', {name: '-'})
+
+  await userEvent.click(decrementButton)
+  expect(Counter.decrement).toHaveBeenCalled
+  expect(Counter.setCount).toHaveBeenCalled
+
+  const counterRender = screen.getByText(/-1/i);
+  expect(counterRender).toBeInTheDocument();
 });
